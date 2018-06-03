0a65de5dd1d3e244a5186f3ad0eec51c3d84cf32
kunal mittal <kanumittal99@gmail.com>
2018-06-03 17:17:31 +0530

    newuser and alert others

--- server/server.js
+++ server/server.js
@@ -10,8 +10,17 @@ var io=socketIO(server);
 app.use(express.static(publicPath));
 // socket.emit for a single connection.
 // io.emit for every single connection.
+//socket.broadcast.emit to everybody except sender.
 io.on('connection',(socket)=>{
     console.log('New User Connected');
+    socket.emit('newMessage',{
+        from:'admin',
+        text:'Welcome to our site.'
+    });
+    socket.broadcast.emit('newMessage',{
+        from:'admin',
+        text:'New buddy in the house!!!'
+    })
     socket.on('createMessage',(newMsg)=>{
         console.log('Create Message',newMsg);
         io.emit('newMessage',{
@@ -19,6 +28,11 @@ io.on('connection',(socket)=>{
             text:newMsg.text,
             createdAt:new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage',{
+        //     from:newMsg.from,
+        //     text:newMsg.text,
+        //     createdAt:new Date().getTime()
+        // });
     })
     socket.on('disconnect',()=>{
         console.log('User DisConnected');
