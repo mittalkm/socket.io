c59976fad87b17d214498f663cb8253c8273eccc
kunal mittal <kanumittal99@gmail.com>
2018-06-03 19:46:06 +0530

    generate message function

--- server/server.js
+++ server/server.js
@@ -7,27 +7,18 @@ const publicPath=path.join(__dirname,'../public');
 const app=express();
 var server=http.createServer(app);
 var io=socketIO(server);
+const {generateMessage}=require('./utils/message.js');
 app.use(express.static(publicPath));
 // socket.emit for a single connection.
 // io.emit for every single connection.
 //socket.broadcast.emit to everybody except sender.
 io.on('connection',(socket)=>{
     console.log('New User Connected');
-    socket.emit('newMessage',{
-        from:'admin',
-        text:'Welcome to our site.'
-    });
-    socket.broadcast.emit('newMessage',{
-        from:'admin',
-        text:'New buddy in the house!!!'
-    })
+    socket.emit('newMessage',generateMessage('admin','Welcome Buddy!'));
+    socket.broadcast.emit('newMessage',generateMessage('admin','New Buddy In The House!'))
     socket.on('createMessage',(newMsg)=>{
         console.log('Create Message',newMsg);
-        io.emit('newMessage',{
-            from:newMsg.from,
-            text:newMsg.text,
-            createdAt:new Date().getTime()
-        });
+        io.emit('newMessage',generateMessage(newMsg.from,newMsg.text));
         // socket.broadcast.emit('newMessage',{
         //     from:newMsg.from,
         //     text:newMsg.text,
