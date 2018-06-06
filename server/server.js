1534b7a9e5f8a71bdd0313b2ea87e9ee234b009e
kunal mittal <kanumittal99@gmail.com>
2018-06-07 01:05:20 +0530

    completed

--- server/server.js
+++ server/server.js
@@ -8,18 +8,35 @@ const app=express();
 var server=http.createServer(app);
 var io=socketIO(server);
 const {generateMessage}=require('./utils/message.js');
+const {Users}=require('./utils/users.js');
 const {generateLocationMessage}=require('./utils/message.js');
+const {isRealString}=require('./utils/validation.js');
 app.use(express.static(publicPath));
+var users=new Users();
 // socket.emit for a single connection.
 // io.emit for every single connection.
 //socket.broadcast.emit to everybody except sender.
 io.on('connection',(socket)=>{
     console.log('New User Connected');
-    socket.emit('newMessage',generateMessage('admin','Welcome Buddy!'));
-    socket.broadcast.emit('newMessage',generateMessage('admin','New Buddy In The House!'))
+    
+    socket.on('join',(params,callback)=>{
+        if(!isRealString(params.name) || !isRealString(params.room)){
+            return callback('Name and Room name are required.');
+        }
+        socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id,params.name,params.room);
+        io.to(params.room).emit('updateUserList',users.getUserList(params.room));
+        socket.emit('newMessage',generateMessage('Admin',`Welcome ${params.name}`));
+        socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} In The House!`));    
+        callback();
+    });
     socket.on('createMessage',(newMsg,callback)=>{
-        console.log('Create Message',newMsg);
-        io.emit('newMessage',generateMessage(newMsg.from,newMsg.text));
+        var user=users.getUser(socket.id);
+        if(user && isRealString(newMsg.text)){
+            io.to(user.room).emit('newMessage',generateMessage(user.name,newMsg.text));
+        }
+        
         callback();
         // socket.broadcast.emit('newMessage',{
         //     from:newMsg.from,
@@ -28,9 +45,18 @@ io.on('connection',(socket)=>{
         // });
     });
     socket.on('createLocationMessage',(coords)=>{
-        io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+        var user=users.getUser(socket.id);
+        if(user){
+            io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude));
+        }
+        //io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
     });
     socket.on('disconnect',()=>{
+        var user=users.removeUser(socket.id);
+        if(user){
+            io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+            io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left.`));
+        }
         console.log('User DisConnected');
     });
 
